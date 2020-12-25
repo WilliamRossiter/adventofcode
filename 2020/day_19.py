c9ca9febace15deae0ecd93fa1c38ffe3ad02b08
WilliamRossiter <62196729+WilliamRossiter@users.noreply.github.com>
2020-12-24 19:28:19 -0800

    day 19 part 1

--- 2020/day_19.py
+++ 2020/day_19.py
@@ -8,18 +8,24 @@ class Input:
             self.messages = inputParts[1].split('\n')
             inputRules = inputParts[0].split('\n')
             self.rules = {}
+            self.regex = ""
+            # ugh, this is gross and is a result of being impatient. Tokenization would make more sense
             for inputRule in inputRules:
-                match = re.match("(\d+): (\d+) (\d+) \| (\d+) (\d)+", inputRule)
+                match = re.match("(\d+): (\d+) (\d+) \| (\d+) (\d+)", inputRule)
                 if match != None:
-                    self.rules[int(match[1])] = [(int(match[2]), int(match[3])), (int(match[4]), int(match[5]))]
+                    self.rules[int(match[1])] = ([int(match[2]), int(match[3])], [int(match[4]), int(match[5])])
+                    continue
+                match = re.match("(\d+): (\d+) \| (\d+)", inputRule)
+                if match != None:
+                    self.rules[int(match[1])] = (int(match[2]), int(match[3]))
                     continue
                 match = re.match("(\d+): (\d+) (\d+) (\d+)", inputRule)
                 if match != None:
-                    self.rules[int(match[1])] = (int(match[2]), int(match[3]), int(match[4]))
+                    self.rules[int(match[1])] = [int(match[2]), int(match[3]), int(match[4])]
                     continue  
                 match = re.match("(\d+): (\d+) (\d+)", inputRule)
                 if match != None:
-                    self.rules[int(match[1])] = (int(match[2]), int(match[3]))
+                    self.rules[int(match[1])] = [int(match[2]), int(match[3])]
                     continue
                 match = re.match("(\d+): (\d+)", inputRule)
                 if match != None:
@@ -29,41 +35,33 @@ class Input:
                 if match != None:
                     self.rules[int(match[1])] = match[2]
                     continue
-
-    def IsValidMessageForRuleIndex(self, message, ruleIndex):
-        rule = self.rules[0]
-        self.IsValidMessageForRule(message, rule)
     
-    def MatchRule(self, message, messageIndex, rule):
-        if (messageIndex >= len(message)):
-            return 0
-        elif type(rule) is str:
-            if (message[messageIndex] == rule):
-                return 1
-            else:
-                return 0
+    def PrintRuleToRegex(self, rule):
+        if type(rule) is str:
+            self.regex += rule
         elif type(rule) is int:
-            ruleIndex = rule
-            return self.MatchRule(message, messageIndex, self.rules[ruleIndex])
-        elif type(rule) is tuple:
-            messageIndexRecursive = messageIndex
-            for ruleIndexRecursive in rule:
-                matchCount = self.MatchRule(message, messageIndexRecursive, self.rules[ruleIndexRecursive])
-                if matchCount == 0:
-                    return 0
-                messageIndexRecursive += matchCount
-            return messageIndexRecursive - messageIndex
+            self.PrintRuleToRegex(self.rules[rule])
         elif type(rule) is list:
-            matchCount = 0
-            for ruleOption in rule:
-                matchCount = self.MatchRule(message, messageIndex, ruleOption)
-                if matchCount != 0:
-                    break
-            return matchCount
+            self.regex += "(?:"
+            for rulePart in rule:
+                self.PrintRuleToRegex(rulePart)
+            self.regex += ")"
+        elif type(rule) is tuple:
+            self.regex += '(?:'
+            self.PrintRuleToRegex(rule[0])
+            self.regex += '|'
+            self.PrintRuleToRegex(rule[1])
+            self.regex += ')'
 
 input = Input('2020/input/day_19.txt')
+for key in input.rules.keys():
+    print(key)
+    print(input.rules[key])
 numValid = 0
+input.PrintRuleToRegex(0)
+print(input.regex)
 for message in input.messages:
-    if input.MatchRule(message, 0, input.rules[0]) == len(message):
+    regex = re.compile("^" + input.regex + "$")
+    if regex.match(message):
         numValid += 1
 print(numValid)
