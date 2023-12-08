ed244bf12a106ab5c7c181e0159a57491283bf80
WilliamRossiter <62196729+WilliamRossiter@users.noreply.github.com>
2023-12-07 21:24:02 -0800

    Update day_4.py

--- 2023/day_4.py
+++ 2023/day_4.py
@@ -5,6 +5,7 @@ import math
 
 class ScratchCard:
     def __init__(self, cardInput):
+        self.copies = 1
         self.winningNumbers = []
         self.myNumbers = []
         match = re.match(r"Card +\d+: ([ \d]+) \| ([ \d]+)", cardInput)
@@ -15,20 +16,31 @@ class ScratchCard:
             if num.isdigit():
                 self.myNumbers.append(int(num))
 
-    def pointValue(self):
-        winningNumbers = 0
+    def matchingNumbers(self):
+        winningNumbersCur = 0
         for number in self.myNumbers:
             if number in self.winningNumbers:
-                winningNumbers += 1
-        return int(2 ** (winningNumbers - 1))
+                winningNumbersCur += 1
+        return winningNumbersCur
+
+    def pointValue(self):
+        return int(2 ** (self.matchingNumbers() - 1))
 
 def Main():
     aryStrInput = common.AryStrFromFile('2023/input/day_4.txt')
     totalPointValue = 0
+    scratchCards = []
+    copiesTotal = 0
     for str in aryStrInput:
-        scratchCard = ScratchCard(str)
-        print(scratchCard.pointValue())
+        scratchCards.append(ScratchCard(str))
+    for index, scratchCard in enumerate(scratchCards):
+        winningNumbersCur = scratchCard.matchingNumbers()
+        copiesCur = scratchCard.copies
+        for i in range(winningNumbersCur):
+            scratchCards[index + i + 1].copies += copiesCur
+        copiesTotal += copiesCur
         totalPointValue += scratchCard.pointValue()
-    print(totalPointValue)
+    print("Day 4_1 Answer: {}".format(totalPointValue))
+    print("Day 4_2 Answer: {}".format(copiesTotal))
 
 Main()
